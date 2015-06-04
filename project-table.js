a5ac43d2c3a78a9aed5bc2878c3cb0cac6d2f6b7
Ashish <itsashis4u@gmail.com>
2015-06-05 03:42:56 +0530

    Till add, list and sorting of tables

--- project-table.js
+++ project-table.js
@@ -1,23 +1,79 @@
+Students = new Mongo.Collection('student');
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+Router.route('/addStudent', function(){
+  this.render('addStudent');
+});
+Router.route('/listStudent', function(){
+  this.render('listStudent');
+});
+  
+  
+  Template.addStudent.events({
+    'click .button': function (event) {
+      event.preventDefault();
+      var d = {};
+      d.name = document.getElementById('full-name').value;
+      d.phone = document.getElementById('phone').value;
+      d.class = document.getElementById('class').value;
+      d.city = document.getElementById('city').value;
+      console.log(JSON.stringify(d));
+
+       Students.insert({
+        name: d.name,
+        phone: d.phone,
+        class: d.class,
+        city: d.city,
+        createdAt: new Date()
+      }, function(error){
+        if(error)
+          console.log(error);
+        else{
+        console.log("Success");
+        window.location.replace('/listStudent');
+      }
+      });  
+
+
     }
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+
+Template.listStudent.helpers({
+  result: function () {
+    var sorter = Session.get('sorter');
+    console.log(sorter)
+
+      if(sorter == undefined)
+        return Students.find();
+
+      if(sorter == 'name-down'){
+       return Students.find({},{sort:{name:1}});
+      }
+      if(sorter == 'name-up'){
+       return Students.find({},{sort:{name:-1}});
+      }
+  },
+  counter: function () {
+      return Students.find().count();
+  }
+
+});
+
+Template.listStudent.events({
+  'click .name.up.icon': function () {
+   Session.set('sorter', 'name-up');
+  },
+  'click .name.down.icon': function () {
+   Session.set('sorter', 'name-down');
+  }
+});
+
 }
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
-}
+// if (Meteor.isServer) {
+//   Meteor.startup(function () {
+//     // code to run on server at startup
+//   });
+// }
